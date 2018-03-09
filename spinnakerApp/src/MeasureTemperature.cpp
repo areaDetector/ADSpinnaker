d4a241b9c92839df1d751b7d348f0ed177b18abc
Mark Rivers <rivers@cars.uchicago.edu>
2018-03-09 08:34:20 -0600

    Changes to illustrate problem with GetBySerial with debug and VS2015

--- spinnakerApp/src/MeasureTemperature.cpp
+++ spinnakerApp/src/MeasureTemperature.cpp
@@ -11,11 +11,15 @@ using namespace std;
   #define Sleep(MS) sleep(MS/1000)
 #endif
 
-#define DEFAULT_SERIAL_NUMBER "17165235"
+#define DEFAULT_TEMPERATURE_LOOPS 5
 
 int main(int argc, char** argv)
 {
-  int result;
+  // Program takes 2 optional arguments
+  // Serial number of camera.  If this argument is absent it uses the first camera in the system.
+  // Number of times to measure temperature.  If this argument is absent it defaults to 5 times.
+  
+  int result = 0;
 	// Retrieve singleton reference to system object
 	SystemPtr system = System::GetInstance();
 
@@ -23,24 +27,35 @@ int main(int argc, char** argv)
 	CameraList camList = system->GetCameras();
 
 	CameraPtr pCam = NULL;
-	const char *serialNumber = DEFAULT_SERIAL_NUMBER;
   
 	try {
     // Select camera
     if (argc > 1) {
-      serialNumber = argv[1];
+      std::string serialNumber = argv[1];
+      cout << "Opening camera serial number: " << serialNumber << endl;
+  	  pCam = camList.GetBySerial(serialNumber);
+    } else {
+      cout << "Opening camera index 0" << endl;
+      pCam = camList.GetByIndex(0);
     }
-    printf("Serial number=%s\n", serialNumber);
-  	pCam = camList.GetBySerial(serialNumber);
+    
+    int numLoops = 5;
+    if (argc > 2) {
+        numLoops = atoi(argv[2]);
+    }
   
   	// Initialize camera
   	pCam->Init();
   		
   	// Retrieve GenICam nodemap
   	INodeMap & nodeMap = pCam->GetNodeMap();
+    CStringPtr pStringNode = (CStringPtr)nodeMap.GetNode("DeviceModelName");
+    cout << "Model name: " << pStringNode->GetValue() << endl;
+    pStringNode = (CStringPtr)nodeMap.GetNode("DeviceSerialNumber");
+    cout << "Serial number: " << pStringNode->GetValue() << endl;
     CFloatPtr pNode = (CFloatPtr)nodeMap.GetNode("DeviceTemperature");
   
-  	for (int i=0; i<100000; i++) {
+  	for (int i=0; i<numLoops; i++) {
   	     printf("Loop = %d, Temperature = %f\n", i, pNode->GetValue());
   	     Sleep(2000);
   	}
@@ -60,5 +75,5 @@ int main(int argc, char** argv)
 	// Release system
 	system->ReleaseInstance();
 
-	return result;
+  return result;
 }
