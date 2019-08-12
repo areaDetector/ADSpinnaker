595511635a1b23cda7fa8c9d90f03bbf8afd957a
Mark Rivers <rivers@cars.uchicago.edu>
2019-08-12 17:37:57 -0500

    Changed DRIVER_VERSION from 1 to 2

--- spinnakerApp/src/ADSpinnaker.cpp
+++ spinnakerApp/src/ADSpinnaker.cpp
@@ -40,7 +40,7 @@ using namespace std;
 #include "SPFeature.h"
 #include "ADSpinnaker.h"
 
-#define DRIVER_VERSION      1
+#define DRIVER_VERSION      2
 #define DRIVER_REVISION     0
 #define DRIVER_MODIFICATION 0
 
